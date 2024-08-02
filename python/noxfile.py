5671cea613a10ea8727a94ed87adb7f763ac6f75
Tobias Fella <fella@posteo.de>
2024-08-02 22:28:45 +0200

    Remove javascript and python bindings

--- python/noxfile.py
+++
@@ -1,8 +0,0 @@
-import nox
-
-
-@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
-def test(session):
-    session.install("-rrequirements-dev.txt")
-    session.install("-e", ".", "--no-build-isolation")
-    session.run("pytest")
