7f7ad945f2a37629755337c65a789e05c706d2b5
Tobias Fella <fella@posteo.de>
2024-08-31 20:27:46 +0200

    Set C++20 in build.rs

--- build.rs
+++ build.rs
@@ -2,5 +2,7 @@ use cxx_build::CFG;
 
 fn main() {
     CFG.include_prefix = "vodozemac";
-    cxx_build::bridge("src/lib.rs").compile("vodozemac");
+    cxx_build::bridge("src/lib.rs")
+        .std("c++20")
+        .compile("vodozemac");
 }
